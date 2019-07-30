db79a6374a960ad14c2ebbc78930b2bbd8962512
japes930 <jpvillaflores9@gmail.com>
2019-07-30 15:31:21 -0400

    initial commit

---
+++ PizzaToppingDemo/src/main/java/com/example/demo/PizzaAnimationApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaAnimationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaAnimationApplication.class, args);
+	}
+
+}
